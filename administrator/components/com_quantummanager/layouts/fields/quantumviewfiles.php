227911df985e47e25cb1edbfbd7008f9388cfedb
tsymb <tsymbal.mitia@gmail.com>
2020-01-09 12:23:09 +0300

    fix lang

--- administrator/components/com_quantummanager/layouts/fields/quantumviewfiles.php
+++ administrator/components/com_quantummanager/layouts/fields/quantumviewfiles.php
@@ -21,7 +21,7 @@ $document = \Joomla\CMS\Factory::getDocument();
             <div class="input-wrapper">
                 <label for="filter-search-<?php echo $id ?>" class="uk-form-icon uk-icon" uk-icon="icon: pencil">
                     <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="search"><circle fill="none" stroke="#000" stroke-width="1.1" cx="9" cy="9" r="7"></circle><path fill="none" stroke="#000" stroke-width="1.1" d="M14,14 L18,18 L14,14 Z"></path></svg>            </label>
-                <input id="filter-search-<?php echo $id ?>" type="text" name="searchqunatumamanger" placeholder="<?php echo Text::_('COM_QUANTUMMANAGER_FIELDS_QUANTUMVIEWFILES_FILTER_NAME'); ?>">
+                <input id="filter-search-<?php echo $id ?>" type="text" name="searchqunatumamanger" placeholder="<?php echo Text::_('COM_QUANTUMMANAGER_FIELD_LABEL_FILTER_NAME'); ?>">
                 <span class="bottom"></span>
                 <span class="right"></span>
                 <span class="top"></span>
