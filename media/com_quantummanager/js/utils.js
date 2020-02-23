7f0f70e0d822080a990836d3a4bf8f983de58399
tsymb <tsymbal.mitia@gmail.com>
2020-02-24 00:38:33 +0300

    fix

--- media/com_quantummanager/js/utils.js
+++ media/com_quantummanager/js/utils.js
@@ -398,6 +398,19 @@ window.QuantumUtils = {
 
             },
         }
+    },
+
+    /**
+     *
+     * @param name
+     * @returns {string}
+     */
+    getUrlParameter: function (name) {
+        name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+        let regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+        let results = regex.exec(location.search);
+        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
     }
+
 
 };
