91a6dccf2fb0523d792cbf486a97e4995d305f89
tsymb <tsymbal.mitia@gmail.com>
2020-03-06 18:56:04 +0300

    fix

--- script.php
+++ script.php
@@ -100,38 +100,4 @@ class Com_QuantummanagerInstallerScript
 
 	}
 
-	/**
-	 * Called after any type of action
-	 *
-	 * @param string           $route   Which action is happening (install|uninstall|discover_install|update)
-	 * @param JAdapterInstance $adapter The object responsible for running this script
-	 *
-	 * @return void True on success
-	 */
-	public function postflight($route, JAdapterInstance $adapter) {}
-
-	/**
-	 * Called on installation
-	 *
-	 * @param JAdapterInstance $adapter The object responsible for running this script
-	 *
-	 * @return void True on success
-	 */
-	public function install(JAdapterInstance $adapter) {}
-
-	/**
-	 * Called on update
-	 *
-	 * @param JAdapterInstance $adapter The object responsible for running this script
-	 *
-	 * @return void True on success
-	 */
-	public function update(JAdapterInstance $adapter) {}
-
-	/**
-	 * Called on uninstallation
-	 *
-	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
-	 */
-	public function uninstall(JAdapterInstance $adapter) {}
 }
