8c737ee52d518c4195da5285dfe026cb73471b93
Dmitrii Cymbal <tsymbal.mitia@gmail.com>
2022-04-19 23:09:16 +0300

    update for joomla4

--- administrator/components/com_quantummanager/layouts/fields/quantumupload.php
+++ administrator/components/com_quantummanager/layouts/fields/quantumupload.php
@@ -13,10 +13,10 @@ use Joomla\CMS\Language\Text;
 $cssClass = $displayData['cssClass'];
 ?>
 
-<div class="quantummanager-module quantummanager-backgrounds-muted quantumupload-module <?php echo $cssClass ?>" data-type="Qantumupload" data-options="maxsize:<?php echo $displayData['maxsize'] ?>;scope:<?php echo $displayData['scope'] ?>;directory:<?php echo $displayData['directory'] ?>;dropAreaHidden:<?php echo $displayData['dropAreaHidden'] ?>">
+<div class="quantummanager-module quantummanager-backgrounds-muted quantumupload-module <?php echo $cssClass ?> <?php if($displayData['dropAreaHidden'] === '1') : ?> qm-hide <?php endif; ?>" data-type="Qantumupload" data-options="maxsize:<?php echo $displayData['maxsize'] ?>;scope:<?php echo $displayData['scope'] ?>;directory:<?php echo $displayData['directory'] ?>;dropAreaHidden:<?php echo $displayData['dropAreaHidden'] ?>">
 
 	<?php $id = mt_rand(11111, 99999); ?>
-    <div class="drop-area <?php if($displayData['dropAreaHidden'] === '1') : ?> drop-area-hidden <?php endif; ?>">
+    <div class="drop-area">
         <div class="form-upload">
             <input type="hidden" class="pathElem" name="path">
             <span class="quantummanager-icon quantummanager-icon-upload"></span>
